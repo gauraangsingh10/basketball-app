70ff187beddc9ee504183bebd5c2abb6f07add73
gauraangsingh10 <gauraangsingh10@gmail.com>
2025-06-26 13:08:02 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+    players = db.relationship('Player', backref="user", lazy=True)
         
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     team = db.relationship('Team', backref='users')
@@ -39,6 +41,8 @@ class Player(db.Model):
     position = db.Column(db.String(20))
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=True)
     stats = db.relationship('Stat', backref='player', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 
 class Game(db.Model):
