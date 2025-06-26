7ba53ce1edad10323f7dcbfedc92584b1cb7d5f2
gauraangsingh10 <gauraangsingh10@gmail.com>
2025-06-26 14:29:02 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from dotenv import load_dotenv
 load_dotenv()  # Load .env before anything else
 
@@ -12,18 +11,3 @@ if __name__ == '__main__':
         db.create_all()
 
     app.run(debug=False)
-=======
-from dotenv import load_dotenv
-load_dotenv()  # Load .env before anything else
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    with app.app_context():
-        from app.extensions import db
-        db.create_all()
-
-    app.run(debug=False)
->>>>>>> 70ff187beddc9ee504183bebd5c2abb6f07add73
