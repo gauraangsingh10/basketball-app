36d8fc999f9fe72ed46470f77b4719b4916179ad
gauraangsingh10 <gauraangsingh10@gmail.com>
2025-06-26 14:15:12 +0530

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -2,9 +2,3 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
-=======
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
